118a15de01e765978e9d8cb0564d633b9c8c3a6f
José <josemartinarance@gmail.com>
2025-01-26 00:25:29 +0100

    feat: Initial commit
    Initial commit of the project, including:
    
    - Basic project structure with activities, fragments, layouts, and resources.
    - Navigation graph setup with fragments for home, second, and third screens.
    - Implementation of basic navigation between fragments.
    - Configuration for build tools, dependencies, and ProGuard rules.
    - Initial styling and theming for the application.

---
+++ app/build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "es.jmarara529.navcomponent_kotlin"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "es.jmarara529.navcomponent_kotlin"
+        minSdk = 34
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    val nav_version = "2.8.3"
+
+    implementation("androidx.navigation:navigation-fragment:$nav_version")
+    implementation("androidx.navigation:navigation-ui:$nav_version")
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
